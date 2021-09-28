dd7013dc7e5e64b33429e7457945b0104083b156
jubaerhossainece <jubaerkuet@gmail.com>
2021-09-28 17:22:57 +0600

    space removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,5 +44,4 @@
     <script src="{{url('assets/vendor/jquery/jquery-3.3.1.min.js')}}"></script>
     <script src="{{url('assets/vendor/bootstrap/js/bootstrap.bundle.js')}}"></script>
 </body>
- 
 </html>
